61868804c170c8f3c0bb965c3f66f551d90e6f0a
doloresjuliana <doloresjuliana@yahoo.es>
2018-12-27 17:38:12 +0100

    Tagging

---
+++ pibiapp/nextcloud/doctype/nextcloud_tags/nextcloud_tags.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, Dolores Juliana and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Nextcloud Tags', {
+	refresh: function(frm) {
+
+	}
+});
